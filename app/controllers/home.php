3430e862850263c32bc47969286ca49f6dacd0e9
massjt <massjt@qq.com>
2017-03-31 00:36:18 +0800

    add view method

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -5,6 +5,9 @@ class Home extends Controller
     {
         $user = $this->model('User');
         $user->name = $name;
-        echo $user->name;
+        //echo $user->name;
+        $this->view('home/index', ['name' => $user->name]);
     }
+
+
 }
--- app/core/Controller.php
+++ app/core/Controller.php
@@ -2,9 +2,20 @@
 
 class Controller
 {
+    /*
+    @param $model  models下的model类
+     */
     protected function model($model)
     {
         require_once '../app/models/' . $model . '.php';
         return new $model();
+    }
+    /*
+    @param $view views下的视图
+    @param $data 传递给视图的参数
+     */
+    protected function view($view, $data = [])
+    {
+        require_once '../app/views/' . $view  . '.php';
     }
 }
